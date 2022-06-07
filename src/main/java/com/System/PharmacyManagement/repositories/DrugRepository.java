87a6d9abcbf34e5e09f00addc6bb6f17dbc23205
LeDucNghia1407 <71742793+LeDucNghia1407@users.noreply.github.com>
2022-06-07 15:46:07 +0700

    Add files via upload

---
+++ src/main/java/com/System/PharmacyManagement/repositories/DrugRepository.java
@@ -0,0 +1,11 @@
+package com.System.PharmacyManagement.repositories;
+
+import com.System.PharmacyManagement.models.Drug;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DrugRepository extends JpaRepository<Drug, Long> {
+    List<Drug> findBydrugName(String drugName);
+
+}
---
+++ src/main/java/com/System/PharmacyManagement/repositories/LoginRepository.java
@@ -0,0 +1,10 @@
+package com.System.PharmacyManagement.repositories;
+
+import com.System.PharmacyManagement.models.Login;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface LoginRepository extends JpaRepository<Login, Long> {
+    List<Login> findByLoginID(String loginID);
+}
