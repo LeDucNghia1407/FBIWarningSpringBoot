caa3b0b41a404944bfbba0a73861ef5fd57b20e6
LeDucNghia1407 <71742793+LeDucNghia1407@users.noreply.github.com>
2022-06-13 17:05:26 +0700

    Delete src/main/java/com/javatechie/jpa/Study directory

--- src/main/java/com/javatechie/jpa/Study/GetHeroes.java
+++
@@ -1,9 +0,0 @@
-package com.javatechie.jpa.Study;
-
-public class GetHeroes {
-    public void getHero()
-    {
-        Heroes h = new Heroes();
-        System.out.println(h.getname(2));
-    }
-}
--- src/main/java/com/javatechie/jpa/Study/Main.java
+++
@@ -1,8 +0,0 @@
-package com.javatechie.jpa.Study;
-
-public class Main {
-    public static void main(String[] args) {
-        GetHeroes get = new GetHeroes();
-        get.getHero();
-    }
-}
